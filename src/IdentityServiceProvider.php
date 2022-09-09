1e38c1aa39acc959539800ac859e3ad0f5f2b158
Dennis Nielsen <62034663+zploited@users.noreply.github.com>
2022-09-09 21:47:05 +0200

    Create IdentityServiceProvider.php

---
+++ src/IdentityServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zploited\Identity\Client\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class IdentityServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
