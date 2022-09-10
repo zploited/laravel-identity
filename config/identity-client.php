21f92a953d57304ede2ed4c23d470dc2309a9600
Dennis Nielsen <62034663+zploited@users.noreply.github.com>
2022-09-10 13:59:28 +0200

    added config to the provider

---
+++ config/identity-client.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'identity' => [
+        'identifier' => env('IDENTITY_IDENTIFIER'),
+        'client_id' => env('IDENTITY_CLIENT_ID'),
+        'client_secret' => env('IDENTITY_CLIENT_SECRET'),
+        'redirect_uri' => env('IDENTITY_REDIRECT_URI')
+    ]
+];
--- src/IdentityServiceProvider.php
+++ src/IdentityServiceProvider.php
@@ -3,16 +3,24 @@
 namespace Zploited\Identity\Client\Laravel;
 
 use Illuminate\Support\ServiceProvider;
+use Zploited\Identity\Client\Identity;
 
 class IdentityServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-
+        $this->publishes([
+            __DIR__.'/../config/identity-client.php' => config_path('identity-client.php')
+        ]);
     }
 
     public function register(): void
     {
+        $this->mergeConfigFrom(__DIR__.'/../config/identity-client.php', 'identity-client');
 
+        // Binding the identity object into a singleton
+        $this->app->singleton(Identity::class, function() {
+            return new Identity(config('identity-client.identity'));
+        });
     }
 }
