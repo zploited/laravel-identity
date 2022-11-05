90c493b7ed8ab5cde633b6fc0bb8c2c41e851765
Dennis Nielsen <62034663+zploited@users.noreply.github.com>
2022-11-05 14:45:45 +0100

    added missing protocol to config

--- config/identity-client.php
+++ config/identity-client.php
@@ -5,7 +5,8 @@ return [
         'client_id' => env('IDENTITY_CLIENT_ID'),
         'client_secret' => env('IDENTITY_CLIENT_SECRET'),
         'redirect_uri' => env('IDENTITY_REDIRECT_URI'),
-        'scopes' => explode(' ', env('IDENTITY_SCOPE'))
+        'scopes' => explode(' ', env('IDENTITY_SCOPE', '')),
+        'protocol' => env('IDENTITY_PROTOCOL', 'https')
     ],
     'guard' => [
         'issuer' => env('IDENTITY_IDENTIFIER')
